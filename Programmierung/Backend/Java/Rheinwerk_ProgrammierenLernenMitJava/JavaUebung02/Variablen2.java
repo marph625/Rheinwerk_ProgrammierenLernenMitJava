d675769103e4a144a9141183d701aed97fd304e2
marph625 <marvin.gnesner@gmail.com>
2024-01-29 11:18:18 +0100

    Variablen2 hinzugefuegt

---
+++ Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/JavaUebung02/Variablen2.java
@@ -0,0 +1,18 @@
+public class Variablen2 {
+	public static void main(String[] args) {
+		
+		//Initialisierung und Zuweisung
+		char z1 = 'a';
+		char z2 = 'b';
+		char z3 = 'A';
+		char z4 = 0x00a9; // Alternativ 169
+		char z5 = 0x00be; // Darstellung in hex. Alternativ 190 dezimal
+
+		//Ausgabe
+		System.out.println("z1 = " + z1);
+		System.out.println("z2 = " + z2);
+		System.out.println("z3 = " + z3);
+		System.out.println("z4 = " + z4);
+		System.out.println("z5 = " + z5);
+	}
+}
