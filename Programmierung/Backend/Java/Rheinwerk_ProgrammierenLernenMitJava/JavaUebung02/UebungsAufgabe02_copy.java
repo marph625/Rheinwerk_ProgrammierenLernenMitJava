7ac6323a8d585430d6a51f9b032f42f0a1e51dfa
marph625 <marvin.gnesner@gmail.com>
2024-01-30 16:18:47 +0100

    bla

---
+++ Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/JavaUebung02/UebungsAufgabe02_copy.java
@@ -0,0 +1,33 @@
+public class UebungsAufgabe02_copy {
+	public static void main(String[] args) {
+		int zahl_der_versuche = 15; // richtig
+		double gehalt = 2645.34; // € falsch, weil anderer Datentyp
+		int hausnummer = 24; // richtig
+		char buchstabe = 'A'; // falsch. 'A' und bezeichner muessen getauscht werden
+		byte b = 127; // falsch, weil byte nur von -128 bis 127 
+		short z = 15; // richtig
+		boolean ist_verheiratet = false; // richtig
+		double laenge, breite, hoehe = 10; // falsch
+		long postleitzahl = 02365; // richtig aber int reicht aus
+		float umfang = 64537.34765f; // falsch, hinter die Zahl muss ein f oder F
+		long zahl = -23456786; 
+		double telefonnummer = 0176.46578675;
+		boolean ist_gerade_zahl = true; // falsch - tausche bezeichner mit 'true'
+		short i = 31556;
+		char zeichen = '\u00b1';
+		byte x = -112;
+		char zeichen2 = 174; 
+		long lange_zahl = 385799;
+		float zahl_1 = 4567.23545f;
+		double verlust = 3456.45; // falsch, für float und double werden '.' anstatt ',' verwendet
+		double zahl1 = -1.7e7;
+		char tabulator = '\t';
+		// char trenner = '\x2f'; // weiß nicht, was hier genau der Fehler ist
+		float m = .3f;
+		char hk = '\'';
+		// double wert = -.e;
+		short zahl13 = 13;
+		double zahl12 = 24;
+	}
+}
+
