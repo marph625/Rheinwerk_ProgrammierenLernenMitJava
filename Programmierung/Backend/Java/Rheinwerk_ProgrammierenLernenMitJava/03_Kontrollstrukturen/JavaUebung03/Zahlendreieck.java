e69b7c4746e6d4462063b37c78b71394c63d4c24
marph625 <marvin.gnesner@gmail.com>
2024-02-04 21:55:17 +0100

    Kommentare hinzugefügt

--- Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/03_Kontrollstrukturen/JavaUebung03/Zahlendreieck.java
+++ Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/03_Kontrollstrukturen/JavaUebung03/Zahlendreieck.java
@@ -1,3 +1,14 @@
+/* 
+ * Die erste Schleife erzeugt solange Zeilen, bis der erste Zähler einen gegebenen Wert erreicht hat
+ *
+ * Die zweite Schleife beginnt bei jedem Durchlauf der ersten Schleife bei 0 und 
+ * gibt solange Ziffern + Leerzeichen aus, bis der innere Zähler den Wert des äußeren
+ * Zählers erreicht hat. Der Wert des inneren Zählers erhöht sich um 1 und gibt demzufolge
+ * in jedem Durchlauf eine Ziffer + Leerzeichen mehr aus, beginnt jedoch immer wieder bei 0
+ * Daraus ergibt sich das gewünschte Zahlendreieck.
+ *
+ */
+
 public class Zahlendreieck {
 	public static void main(String[] args) {
 		var counter_outer = 0;
