ff229f181421bfe5b151eb6dfb70069fb040d45c
marph625 <marvin.gnesner@gmail.com>
2024-02-04 23:20:12 +0100

    Euklid, baby!

---
+++ Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/03_Kontrollstrukturen/JavaUebung03/ggtEuklid.java
@@ -0,0 +1,24 @@
+/*
+ * Algorithmus zum Berechnen des größten gemeinsamen Teilers zu Ehren des Euklid
+ */
+
+import javax.swing.JOptionPane;
+
+public class ggtEuklid{
+	public static void main(String[] whatever) {
+		int m, n, r;
+		var eingabe_m = JOptionPane.showInputDialog("m = ");
+		var eingabe_n = JOptionPane.showInputDialog("n = ");
+
+		m = Integer.parseInt(eingabe_m);
+		n = Integer.parseInt(eingabe_n);
+		r = m % n;
+
+		while (r > 0) {
+			m = n;
+			n = r;
+			r = m % n;
+		}
+		JOptionPane.showMessageDialog(null, "n = " + n);
+	}
+}
