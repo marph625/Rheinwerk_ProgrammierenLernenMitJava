6833f5ac757c4245d5db644c367e0582c65bb705
marph625 <marvin.gnesner@gmail.com>
2024-02-05 16:12:39 +0100

    Collatzfolge klappt wie gewünscht (musste aber mal in die Lösung luschern -_- Struktogramm hat geholfen)

---
+++ Programmierung/Backend/Java/Rheinwerk_ProgrammierenLernenMitJava/03_Kontrollstrukturen/JavaUebung03/Collatzfolge.java
@@ -0,0 +1,24 @@
+/*
+ * Programm zur Berechnung einer Collatzfolge
+ */
+
+
+import javax.swing.JOptionPane;
+
+public class Collatzfolge {
+        public static void main(String[] args) {
+                var eingabe_zahl = JOptionPane.showInputDialog("Gib eine Zahl ein");
+                int xn = Integer.parseInt(eingabe_zahl);
+		System.out.print(xn + " ");
+		do {
+			if (xn % 2 == 0) {
+				xn = xn / 2;
+				System.out.print(xn + " ");
+			} else {
+				xn = 3 * xn + 1;
+				System.out.print(xn + " ");
+			}
+		} while (xn != 1);
+                
+        }
+}
